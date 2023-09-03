5c961b7d10b784554d9db6cc0675b68d822b7007
Igor Mihejev <Igor.Mihejev@gmail.com>
2023-09-03 07:56:19 +0300

    Fixed mini purchase

--- contracts/TradableMiniature.sol
+++ contracts/TradableMiniature.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.9;
 //import erc721 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 
 // Smart contract for a tradable miniature NTF
 struct  Miniature {
@@ -14,27 +15,58 @@ struct  Miniature {
 }
 
 contract TradableMiniature is ERC721 {
-
-  // Array of all the miniatures
-  Miniature[] public miniatures;
-  mapping(address => Miniature[]) public miniaturesByOwner;
+  using Counters for Counters.Counter;
+  Counters.Counter private _tokenIds;
+  Miniature[] public miniatures; 
 
   constructor() ERC721("TradableMiniature", "TDMT") {
+    _tokenIds.increment();
   }
 
-  function createMiniature(string memory _name, string memory _description, string memory _url, uint256 _price) public {
-    Miniature memory newMiniature = Miniature(_name, _description, _url, _price, miniatures.length, msg.sender);
+  function createMiniature(string memory _name, string memory _description, string memory _url, uint256 _price) public returns(uint256) {
+    uint newId = _tokenIds.current();
+    Miniature memory newMiniature = Miniature(_name, _description, _url, _price, newId, msg.sender);
     miniatures.push(newMiniature);
-    miniaturesByOwner[msg.sender].push(newMiniature);
     _safeMint(msg.sender, miniatures.length);
+    _tokenIds.increment();
+    return newId;
+  }
+
+  function buyMiniature(uint256 _index) public payable {
+    Miniature memory miniature = miniatures[_index];
+    require(msg.value == miniature.price, "Price not met");
+    require(msg.value >= miniature.price, "Not enough funds sent");
+    require(miniature.owner != msg.sender, "You already own this miniature");
+    payable(miniature.owner).transfer(msg.value);
+    _transfer(miniature.owner, msg.sender, miniature.id);
+    miniature.owner = msg.sender;
+    miniatures[_index] = miniature;
   }
 
   function getAllMiniatures() public view returns (Miniature[] memory) {
     return miniatures;
   }
 
+  function getMiniatureIndex(uint256 _id) public view returns (uint256) {
+    for(uint256 i = 0; i < miniatures.length; i++) {
+      if(miniatures[i].id == _id) {
+        return i;
+      }
+    }
+    revert("Miniature not found");
+  }
+
   function getOwnedMiniatures() public view returns (Miniature[] memory) {
-    return miniaturesByOwner[msg.sender];
+    Miniature[] memory ownedMiniatures = new Miniature[](balanceOf(msg.sender));
+    uint256 index = 0;
+    for (uint256 i = 0; i < miniatures.length; i++) {
+        if (miniatures[i].owner == msg.sender) {
+            ownedMiniatures[index] = miniatures[i];
+            index++;
+        }
+    }
+
+    return ownedMiniatures;
   }
 
 }
--- test/TradableMiniatures.ts
+++ test/TradableMiniatures.ts
@@ -3,16 +3,16 @@ import { ethers } from "hardhat";
 
 describe("TradableMiniature", function() {
   async function deployContract() {
-    const [owner, anotherAddress] = await ethers.getSigners();
+    const [owner, anotherUser] = await ethers.getSigners();
 
     const TradableMiniature = await ethers.getContractFactory("TradableMiniature");
     const tradableMiniature = await TradableMiniature.deploy();
-    return { tradableMiniature, owner, anotherAddress };
+    return { tradableMiniature, owner, anotherUser };
   }
 
   describe("Minting Miniature", async function() {
     it("Should mint a new miniature", async function() { 
-      const { tradableMiniature, owner, anotherAddress: minter } = await deployContract();
+      const { tradableMiniature, owner, anotherUser: minter } = await deployContract();
       const miniatureMetadata = {
         name: "Miniature",
         description: "A miniature",
@@ -30,6 +30,34 @@ describe("TradableMiniature", function() {
       )
 
       const ownedMiniatures = await connectedMinter.getOwnedMiniatures();
+      expect(ownedMiniatures.length).to.equal(1);
+      expect(ownedMiniatures[0].name).to.equal(miniatureMetadata.name);
+    })
+  })
+
+  describe("Buying Miniature", async function() {
+    it("Should buy a miniature", async function() {
+      const { tradableMiniature, owner, anotherUser: minter } = await deployContract();
+      const miniatureMetadata = {
+        name: "Miniature",
+        description: "A miniature",
+        url: "https://example.com/image.png",
+        price: 100
+      }
+      const connectedMinter = tradableMiniature.connect(owner);
+      const connectedBuyer = tradableMiniature.connect(minter);
+ 
+      await connectedMinter.createMiniature(
+        miniatureMetadata.name,
+        miniatureMetadata.description,
+        miniatureMetadata.url,
+        miniatureMetadata.price
+      );
+
+      const miniatureIndex = await connectedBuyer.getMiniatureIndex(1);
+      await connectedBuyer.buyMiniature(miniatureIndex, { value: miniatureMetadata.price });
+
+      const ownedMiniatures = await connectedBuyer.getOwnedMiniatures();
       expect(ownedMiniatures.length).to.equal(1);
       expect(ownedMiniatures[0].name).to.equal(miniatureMetadata.name);
     })
